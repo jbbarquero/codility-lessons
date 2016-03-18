cc0aa45726c6d000de2664f6d5f3568fa1c3d8ff
Javier Beneito Barquero <jbbarquero@gmail.com>
2016-03-19 00:11:27 +0100

    codility-0000: project createdf

---
+++ src/test/java/com/malsolo/codility/AppTest.java
@@ -0,0 +1,13 @@
+package com.malsolo.codility;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+        assertTrue( true );
+    }
+}
